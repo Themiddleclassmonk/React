eb468b72f7096a04641321765a91fcf7e6d17083
Shubham <shubhamkumar1582003@gmail.com>
2026-05-24 11:25:12 +0530

    TWB-Text

--- React_part_2/src/App.jsx
+++ React_part_2/src/App.jsx
@@ -1,11 +1,36 @@
+// Two way binding in forms 
+
 import React from 'react'
+import { useState } from 'react'
+
+
 
 const App = () => {
+
+  const [Name, setName] = useState("");
+
+  console.log(Name)
+
+  const PreventReload = (e) =>{
+    e.preventDefault();
+  }
+
+
   return (
     <>
-     <h1>Food</h1> 
+      <form onSubmit={PreventReload}>
+        <input
+          onChange={(e) => {
+    setName(e.target.value)
+  }}
+          type="text"
+          placeholder="Name :"
+          value={Name}
+        />
+        <button> Submit </button>
+      </form>
     </>
-  )
+  );
 }
 
 export default App
