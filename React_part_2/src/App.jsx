6c7beddfee2752f98ecb9ae7ac347628989896bc
Shubham <shubhamkumar1582003@gmail.com>
2026-05-24 14:12:01 +0530

    two way binding -checkboxes and radio button

--- React_part_2/src/App.jsx
+++ React_part_2/src/App.jsx
@@ -1,36 +1,57 @@
-// Two way binding in forms 
+// Two way binding in forms - check boxes and radio  
 
 import React from 'react'
-import { useState } from 'react'
-
-
+import { useState } from 'react';
 
 const App = () => {
-
-  const [Name, setName] = useState("");
-
-  console.log(Name)
 
   const PreventReload = (e) =>{
     e.preventDefault();
   }
 
-
+  const [completed, setcompleted] = useState(true);
+  const [Gender, setGender] = useState("Male")
   return (
     <>
+      <h1>Two way binding - check boxes and radio buttons .</h1>
       <form onSubmit={PreventReload}>
         <input
+          value="Male"
           onChange={(e) => {
-    setName(e.target.value)
-  }}
-          type="text"
-          placeholder="Name :"
-          value={Name}
+            setGender(e.target.value);
+          }}
+          type="radio"
+          checked ={Gender == "Male" && true}
         />
-        <button> Submit </button>
+        Male
+        <br />
+        <br />
+        <input
+          value="Female"
+          onChange={(e) => {
+            setGender(e.target.value);
+          }}
+          type="radio"
+          checked = {Gender == "Female" && true}
+        />
+        Female
+        <br />
+        <br />
+        <input
+          checked = {completed}
+          onChange={(e) => {
+            setcompleted(e.target.checked);
+          }}
+          type="checkbox"
+        />
+        completed
+        <br />
+        <br />
+        <button> submit </button>
       </form>
     </>
   );
 }
 
 export default App
+
